833f7bace10cc757e93570ca8a91d07a3af52aba
Yoad Duani <yoad787@gmail.com>
2021-09-12 17:37:23 +0300

    splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/screens/main_screen.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'bloc/get_top_headlines_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+MainScreen neWMainScreen = new MainScreen();
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     // return MaterialApp(
+//     //   // title: 'Flutter Demo',
+//     //   // theme: ThemeData(
+//     //   //   primarySwatch: Colors.blue,
+//     //   // ),
+//     //   home: MainScreen(),
+//     // );
+//     return SplashScreen(
+//         seconds: 14,
+//         navigateAfterSeconds: MainScreen(),
+//         title: new Text('Welcome In SplashScreen'),
+//         image: new Image.asset('assets/img/placeholder.jpg'),
+//         backgroundColor: Colors.white,
+//         styleTextUnderTheLoader: new TextStyle(),
+//         photoSize: 100.0,
+//         loaderColor: Colors.red);
+//   }
+// }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Future<dynamic> loadFromFuture() async {
+  //   getTopHeadlinesBloc..getHeadlines();
+  //   return Future.value(MainScreen());
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MainScreen(),
+      home: SplashScreen(
+          seconds: 4,
+          navigateAfterSeconds: neWMainScreen,
+          // navigateAfterFuture: loadFromFuture(),
+          title: new Text(
+            'News Application',
+            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
+          image: new Image.asset('assets/img/Y.D.png'),
+          backgroundColor: Colors.white,
+          styleTextUnderTheLoader: new TextStyle(),
+          photoSize: 100.0,
+          onClick: () => print("Flutter Egypt"),
+          loaderColor: Colors.red),
     );
   }
 }
